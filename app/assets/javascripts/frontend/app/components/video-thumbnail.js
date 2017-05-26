97dd0021a3bb4a8ddb507f30db93657db213f7b6
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-05-26 18:21:30 +0200

    [TRAILERS] Clicking on a thumbnail refreshes the player and plays a nex trailer.

--- app/assets/javascripts/frontend/app/components/video-thumbnail.js
+++ app/assets/javascripts/frontend/app/components/video-thumbnail.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    renderVideoComponent(videoUrl) {
+      var video = $('#videoPlayer source');
+      $(video).pause();
+      $(video).attr('src', videoUrl);
+      $(video).load();
+      $(video).play();
+    }
+  }
 });
